3e1e3395931fb7045ccb117d35453f332ae74f51
Mirziyod <mirsaidovmirziyod1507@gmail.com>
2023-05-24 21:01:43 +0500

    done

--- main.js
+++ main.js
@@ -1,39 +1,51 @@
 let form = document.querySelector(".form");
-let input = document.querySelector(".form__input");
+let input = document.querySelector(".cardType");
 let formLogo = document.querySelector(".form__logo");
-let img = document.querySelector("img");
-let arr = [];
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
+let img = document.querySelector(".loogo");
+let bankType = document.querySelector(".bankType");
+let bankTypeTitle = document.querySelector(".card__title");
 
+input.addEventListener("keyup", function (evt) {
   let inputVal = input.value;
 
-  arr.push(inputVal);
-  for (let i = 0; i < inputVal.length; i++) {
-    if ((arr[(0, 1, 2, 3)] === "9", "8", "6", "0")) {
-      formLogo.img = document.createElement("img");
-      formLogo.img.src = "./exam/humo.png";
-      src = formLogo;
-      src.appendChild(formLogo.img);
-    } else if ((arr[(0, 1, 2, 3)] == "8", "6", "0", "0")) {
-      formLogo.img = document.createElement("img");
-      formLogo.img.src = "./exam/Uzcard_Logo_.png";
-      src = formLogo;
-      src.appendChild(formLogo.img);
-    } else if ((arr[(0, 1, 2, 3)] == "5", "3", "0", "0")) {
-      formLogo.img = document.createElement("img");
-      formLogo.img.src = "./exam/humo.png";
-      src = formLogo;
-      src.appendChild(formLogo.img);
-    } else if ((arr[(0, 1, 2, 3)] == "4", "2", "7", "8")) {
-      formLogo.img = document.createElement("img");
-      formLogo.img.src = "./exam/humo.png";
-      src = formLogo;
-      src.appendChild(formLogo.img);
-    } else {
-      console.log(false);
-    }
+  if (inputVal === "9860") {
+    img.setAttribute("src", "./exam/humo.png");
+  } else if (inputVal == "8600") {
+    img.setAttribute("src", "./exam/Uzcard_Logo_.png");
+  } else if (inputVal == "5300") {
+    img.setAttribute("src", "./exam/MasterCard_early_1990s_logo.png");
+  } else if (inputVal == "4278") {
+    img.setAttribute("src", "./exam/58482363cef1014c0b5e49c1.png");
+  } else {
+    img.setAttribute("src", "");
   }
 
   console.log(inputVal, arr);
+});
+
+bankType.addEventListener("keyup", function (evt) {
+  let bankTypeVal = bankType.value;
+
+  switch (bankTypeVal) {
+    case "1650":
+      bankTypeTitle.textContent = "Kapital Bank";
+      break;
+    case "2742":
+      bankTypeTitle.textContent = "NBU";
+      break;
+    case "3572":
+      bankTypeTitle.textContent = "TBC";
+      break;
+    case "1660":
+      bankTypeTitle.textContent = "OFB";
+      break;
+    case "7532":
+      bankTypeTitle.textContent = "SQB";
+      break;
+    case "1135":
+      bankTypeTitle.textContent = "Xalq banki";
+      break;
+    default:
+      bankType.textContent = "Your Bank";
+  }
 });
